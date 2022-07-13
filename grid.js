585100e3f2ca420234d90fafb3bc01be9d89ca7c
9ziggy9 <davidarogers@protonmail.com>
2022-07-13 12:22:54 -0700

    boundaries added

--- grid.js
+++ grid.js
@@ -15,7 +15,7 @@ canvas.height = HEIGHT;
 // GLOBAL STATE
 class State {
   constructor() {
-    this.mode = 'obstacles';
+    this.mode = 'boundaries';
 
     this.grid = new Grid();
     Grid.init(this.grid);
@@ -29,12 +29,13 @@ class State {
 // CLASSES
 class Grid {
   constructor() {
-    this.cells = [];
+    this.cells = new Array(ROWS);
+    for (let y = 0; y < ROWS; y++) this.cells[y] = new Array(COLS);
   }
   static init(grid) {
-    for (let y = 0; y < HEIGHT; y += CELL_SIZE) {
-      for (let x = 0;  x < WIDTH; x += CELL_SIZE) {
-	grid.cells.push(new Cell(x,y));
+    for (let y = 0; y < ROWS; y++) {
+      for (let x = 0;  x < COLS; x++) {
+	grid.cells[y][x] = new Cell(CELL_SIZE*x,CELL_SIZE*y);
       }
     }
   }
@@ -47,6 +48,8 @@ class Cell {
     this.width = CELL_SIZE;
     this.height = CELL_SIZE;
     this.isAccessible = true;
+    // 0 -> accessible, 1 -> boundary, 2 -> start, 3 -> end, 4 -> path
+    this.type = 0; 
   }
   static collision(first, second) {
     if (!(first.x > second.x + second.width
@@ -57,11 +60,33 @@ class Cell {
     }
     return false;
   }
-  draw() {
+  highlight() {
     if (mouse.x && mouse.y && Cell.collision(this, mouse)) {
       ctx.strokeStyle = 'green';
       ctx.strokeRect(this.x, this.y, this.width, this.height);
     }
+  }
+  draw() {
+    switch(this.type) {
+    case 0:
+      ctx.fillStyle = '#999999'; // light gray
+      break;
+    case 1:
+      ctx.fillStyle = '#333333'; // charcoal
+      break;
+    case 2:
+      ctx.fillStyle = 'blue';
+      break;
+    case 3:
+      ctx.fillStyle = 'red';
+      break;
+    case 4:
+      ctx.fillStyle = 'green';
+      break;
+    default:
+      break;
+    }
+    ctx.fillRect(this.x, this.y, this.width, this.height);
   }
 }
 
@@ -85,16 +110,37 @@ canvas.addEventListener('mouseleave', e => {
   mouse.x = undefined;
   mouse.y = undefined;
 });
+canvas.addEventListener('click', e => {
+  const x = (mouse.x - (mouse.x % CELL_SIZE)) / CELL_SIZE;
+  const y = (mouse.y - (mouse.y % CELL_SIZE)) / CELL_SIZE;
+  console.log(x,y);
+  switch(state.mode) {
+  // types: 0 -> accessible, 1 -> boundary, 2 -> start, 3 -> end, 4 -> path
+  case 'boundaries':
+    state.grid.cells[y][x].isAccessible = false;
+    state.grid.cells[y][x].type = 1;
+    console.log(state.grid.cells[y][x]);
+    break;
+  default:
+    break;
+  }
+});
 
 // INIT
 const state = new State();
 console.log(state.grid.cells);
 
 // HANDLING
-const handleGrid = () => state.grid.cells.forEach(cell => cell.draw());
+const handleHighlight = () => state.grid.cells.forEach(row => {
+  row.forEach(cell => cell.highlight());
+});
+const handleGrid = () => state.grid.cells.forEach(row => {
+  row.forEach(cell => cell.draw());
+});
 
 function run() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
+  handleHighlight();
   handleGrid();
   requestAnimationFrame(run);
 }
